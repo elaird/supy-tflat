26ce07acdbeecb5a6d68ae64339102fc2218cb6b
Edward Laird <elaird@gmail.com>
2013-12-03 10:44:26 -0600

    displays

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import supy
+import displayer
 import calculables
 import ROOT as r
 
@@ -6,10 +7,11 @@ class example(supy.analysis):
     
     def listOfSteps(self, pars):
         return [supy.steps.printer.progressPrinter(),
-                supy.steps.histos.multiplicity("tauPt"),
-                supy.steps.filters.multiplicity("tauPt", min=2, max=2),
-                supy.steps.histos.multiplicity("bPt"),
-                supy.steps.filters.multiplicity("bPt", min=2, max=2),
+                #supy.steps.histos.multiplicity("tauPt"),
+                #supy.steps.filters.multiplicity("tauPt", min=2, max=2),
+                #supy.steps.histos.multiplicity("bPt"),
+                #supy.steps.filters.multiplicity("bPt", min=2, max=2),
+                displayer.displayer(),
                 ]
 
 
@@ -23,7 +25,8 @@ class example(supy.analysis):
         h = supy.samples.SampleHolder()
         # xs in pb
         h.add("H300_hh_bbtautau",
-              '["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/gg/analysis_signal.root"]',
+              #'["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/gg/analysis_signal.root"]',
+              '["/afs/hep.wisc.edu/home/elaird/analysis.root"]',
               xs=0.0159)
         h.add("ZZ_2l2q",
               '["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/zz/analysis_zz.root"]',
@@ -39,9 +42,9 @@ class example(supy.analysis):
 
         w = calculables.LastBinOverFirstBin(dir="TT", histoName="results")
 
-        return (specify(names="H300_hh_bbtautau", weights=w, color=r.kBlue, nFilesMax=None) +
-                specify(names="ZZ_2l2q", weights=w, color=r.kRed, nFilesMax=None) +
-                specify(names="tt_bbll", weights=w, color=28, nFilesMax=None) +
+        return (specify(names="H300_hh_bbtautau", color=r.kBlue, nFilesMax=None) +
+                #specify(names="ZZ_2l2q", weights=w, color=r.kRed, nFilesMax=None) +
+                #specify(names="tt_bbll", weights=w, color=28, nFilesMax=None) +
                 []
                 )
 
