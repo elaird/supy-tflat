71b1a1e270295e228592acc7315479c93a30b328
Edward Laird <elaird@gmail.com>
2013-12-05 08:58:48 -0600

    loop over tchain files

--- calculables.py
+++ calculables.py
@@ -1,23 +1,35 @@
 import supy
+import ROOT as r
 
 class LastBinOverFirstBin(supy.wrappedChain.calculable):
-    def __init__(self, dir="", histoName=""):
-        self.dir = dir
-        self.histoName = histoName
+    def __init__(self, dir="", histoName="", firstBin=1):
+        self.hPath = "%s/%s" % (dir, histoName)
+        self.firstBin = firstBin
         self.value = None
+
+    def histo(self, elements):
+        # http://root.cern.ch/root/html/TChain.html#TChain:AddFile
+        out = None
+        for iElement in range(elements.GetEntries()):
+            element = elements.At(iElement)
+            f = r.TFile(element.GetTitle())
+            assert f
+            h = f.Get(self.hPath)
+            assert h, "Histogram %s not found." % self.hPath
+            if out:
+                out.Add(h)
+            else:
+                out = h.Clone()
+                out.SetDirectory(0)
+            f.Close()
+        return out
 
     def update(self, _):
         if self.value is not None:
             return
 
-        nTrees = self.source["chain"].GetNtrees()
-        assert nTrees == 1, "Chain has %d trees.  Consider using hadd." % nTrees
-        f = self.source["chain"].GetFile()
-        assert f
-        hPath = "%s/%s" % (self.dir, self.histoName)
-        h = f.Get(hPath)
-        assert h, "Histogram %s not found." % hPath
+        h = self.histo(self.source["chain"].GetListOfFiles())
         num = h.GetBinContent(h.GetNbinsX())
-        den = h.GetBinContent(1)
+        den = h.GetBinContent(self.firstBin)
         assert den
         self.value = num / den
