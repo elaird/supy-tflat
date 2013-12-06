542474811721f39b9909fe545d36bcec5876b43a
Edward Laird <elaird@gmail.com>
2013-12-06 07:35:30 -0600

    some calculables

--- calculables.py
+++ calculables.py
@@ -37,3 +37,79 @@ class LastBinOverFirstBin(supy.wrappedChain.calculable):
         den = h.GetBinContent(self.firstBin)
         assert den
         self.value = num / den
+
+
+class jets(supy.wrappedChain.calculable):
+    @property
+    def name(self):
+        return self.__name
+
+    def __init__(self, var="", keys=[], sortBy="", nBranches=None, reverse=False, ptMin=None, absEtaMax=None):
+        self.var = var
+        self.keys = keys
+        self.nBranches = nBranches
+        self.lvs = [{"p4": supy.utils.LorentzV()} for i in range(self.nBranches)]
+        assert self.var
+
+        self.ptMin = ptMin
+        self.absEtaMax = absEtaMax
+        self.moreName = "%g < pT; |eta| < %g" % (self.ptMin, self.absEtaMax)
+
+        self.sortBy = sortBy
+        self.reverse = reverse
+
+        self.__name = self.var + "s"
+        if self.sortBy:
+            self.__name += "_%sSorted" % self.sortBy
+
+    def update(self, _):
+        self.value = []
+        for iJet in range(self.nBranches):
+            coords = [self.source["%s%d%s" % (self.var, 1+iJet, s)] for s in ["Pt", "Eta", "Phi", "Mass"]]
+            self.lvs[iJet]["p4"].SetCoordinates(*tuple(coords))
+            if self.ptMin and (coords[0] < self.ptMin):
+                continue
+            if self.absEtaMax and (self.absEtaMax < abs(coords[1])):
+                continue
+
+            for key in self.keys:
+                self.lvs[iJet][key] = self.source["%s%d%s" % (self.var, 1+iJet, key)]
+            self.value.append(self.lvs[iJet])
+
+        if self.sortBy:
+            self.value = sorted(self.value, key=lambda x:x[self.sortBy], reverse=self.reverse)
+
+
+class indexedVar(supy.wrappedChain.calculable):
+    @property
+    def name(self):
+        return "%s_%s%d" % (self.var, self.key, self.index)
+
+    def __init__(self, var="", key="", index=None):
+        self.var = var
+        self.key = key
+        self.index = index
+        assert self.index is not None
+
+    def update(self, _):
+        self.value = self.source[self.var][self.index][self.key]
+
+
+class diJetP4(supy.wrappedChain.calculable):
+    def __init__(self, var=""):
+        self.var = var
+
+    def update(self, _):
+        self.value = self.source[self.var][0]["p4"]+self.source[self.var][1]["p4"]
+
+
+class diTauP4(supy.wrappedChain.calculable):
+    def __init__(self, index=None):
+        self.index = index
+        self.lvs = [supy.utils.LorentzV(), supy.utils.LorentzV()]
+
+    def update(self, _):
+        for i in range(2):
+            coords = [self.source["%s%d" % (s, 1+i)].at(self.index) for s in ["pt", "eta", "phi", "m"]]
+            self.lvs[i].SetCoordinates(*tuple(coords))
+        self.value = self.lvs[0] + self.lvs[1]
