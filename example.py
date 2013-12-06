ee1eda2a366b194ca8e4bd3c1941020db9fea845
Edward Laird <elaird@gmail.com>
2013-12-06 03:19:58 -0600

    high stats samples

--- example.py
+++ example.py
@@ -26,18 +26,17 @@ class example(supy.analysis):
         # xs in pb
         h.add("H300_hh_bbtautau",
               #'["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/gg/analysis_signal.root"]',
-              '["/scratch/zmao/analysis.root"]',
+              #'["/scratch/zmao/analysis.root"]',
+              'utils.fileListFromDisk("/hdfs/store/user/zmao/H2hh-SUB-TT/")',
               xs=0.0159)
-        h.add("ZZ_2l2q",
-              '["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/zz/analysis_zz.root"]',
-              xs=2.5)
 
-        h.add("ZZ_2l2q_100",
-              'utils.fileListFromDisk("/hdfs/store/user/zmao/ZZ-SUB-TT/")',
+        h.add("ZZ_2l2q",
+              'utils.fileListFromDisk("/hdfs/store/user/zmao/ZZ2-SUB-TT/")',
               xs=2.5)
 
         h.add("tt_bbll",
-              '["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/tt/analysis_tt.root"]',
+              #'["/afs/hep.wisc.edu/home/zmao/CMSSW_5_3_7/src/UWAnalysis/CRAB/LTau/tt/analysis_tt.root"]',
+              'utils.fileListFromDisk("/hdfs/store/user/zmao/tt-SUB-TT/")',
               xs=26.1975)
         return [h]
 
@@ -46,12 +45,13 @@ class example(supy.analysis):
         from supy.samples import specify
 
         w = calculables.LastBinOverFirstBin(dir="TT", histoName="results")
-        w2 = calculables.LastBinOverFirstBin(dir="TT", histoName="results", firstBin=2)
 
-        return (#specify(names="H300_hh_bbtautau", color=r.kBlue, nFilesMax=None) +
-                specify(names="ZZ_2l2q_100", weights=w2, color=r.kRed, nFilesMax=2) +
-                #specify(names="ZZ_2l2q", weights=w, color=r.kRed, nFilesMax=None) +
-                #specify(names="tt_bbll", weights=w, color=28, nFilesMax=None) +
+        kargs = {"weights": w,
+                 "nFilesMax": 10,
+                 }
+        return (specify(names="H300_hh_bbtautau", color=r.kBlue, **kargs) +
+                specify(names="ZZ_2l2q", color=r.kRed, **kargs) +
+                specify(names="tt_bbll", color=28, **kargs) +
                 []
                 )
 
@@ -65,9 +65,9 @@ class example(supy.analysis):
         for sample, color in [("H300_hh_bbtautau", r.kBlue),
                               ("ZZ_2l2q", r.kRed),
                               ("tt_bbll", 28)]:
-            org.mergeSamples(targetSpec=gopts(sample, color), sources=[sample+".LastBinOverFirstBin"])
+            org.mergeSamples(targetSpec=gopts(sample, color), sources=[sample+".LastBinOverBin1"])
 
-        org.scale(1.0e3) # pb
+        org.scale(20.0e3) # pb
         #org.scale(toPdf=True)
 
         supy.plotter(org,
