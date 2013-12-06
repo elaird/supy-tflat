485d23c60a604ca1c1f7dd9af37db9ddc2800bc5
Edward Laird <elaird@gmail.com>
2013-12-06 07:35:55 -0600

    skeleton

--- example.py
+++ example.py
@@ -9,8 +9,33 @@ class example(supy.analysis):
         return [supy.steps.printer.progressPrinter(),
                 #supy.steps.histos.multiplicity("genTauPt"),
                 #supy.steps.filters.multiplicity("genTauPt", min=2, max=2),
-                supy.steps.histos.multiplicity("genBPt"),
+                #supy.steps.histos.multiplicity("genBPt"),
                 #supy.steps.filters.multiplicity("genBPt", min=2, max=2),
+
+                supy.steps.histos.multiplicity("Js_CSVbtagSorted"),
+                supy.steps.filters.multiplicity("Js_CSVbtagSorted", min=2),
+
+                supy.steps.histos.value("Js_CSVbtagSorted_CSVbtag0", 20, 0.0, 1.0),
+                supy.steps.filters.value("Js_CSVbtagSorted_CSVbtag0", min=0.679),
+
+                supy.steps.histos.value("Js_CSVbtagSorted_CSVbtag1", 20, 0.0, 1.0),
+                supy.steps.filters.value("Js_CSVbtagSorted_CSVbtag1", min=0.244),
+
+                supy.steps.histos.mass("diJetP4", 20, 0.0, 200.0),
+                supy.steps.filters.mass("diJetP4", min=100.0, max=140.0),
+
+                supy.steps.histos.multiplicity("pt1"),
+                supy.steps.filters.multiplicity("pt1", min=1),
+
+                #supy.steps.histos.value("svMass", 20, 0.0, 200.0),
+                #supy.steps.histos.value("svPt", 20, 0.0, 200.0),
+                supy.steps.histos.value("met", 20, 0.0, 200.0),
+
+                supy.steps.histos.mass("diTauP4", 20, 0.0, 200.0),
+                supy.steps.filters.mass("diTauP4", min=50.0, max=130.0),
+
+                supy.steps.histos.pt("diTauP4", 20, 0.0, 200.0),
+
                 #displayer.displayer(),
                 ]
 
@@ -18,6 +43,13 @@ class example(supy.analysis):
     def listOfCalculables(self, pars):
         out = supy.calculables.zeroArgs(supy.calculables)
         #out += supy.calculables.zeroArgs(calculables)
+        out += [calculables.jets(var="J", nBranches=4, ptMin=20.0, absEtaMax=2.4,
+                                 keys=["CSVbtag"], sortBy="CSVbtag", reverse=True),
+                calculables.indexedVar(index=0, var="Js_CSVbtagSorted", key="CSVbtag"),
+                calculables.indexedVar(index=1, var="Js_CSVbtagSorted", key="CSVbtag"),
+                calculables.diJetP4(var="Js_CSVbtagSorted"),
+                calculables.diTauP4(index=0),
+                ]
         return out
 
 
@@ -46,12 +78,10 @@ class example(supy.analysis):
 
         w = calculables.LastBinOverFirstBin(dir="TT", histoName="results")
 
-        kargs = {"weights": w,
-                 "nFilesMax": 10,
-                 }
-        return (specify(names="H300_hh_bbtautau", color=r.kBlue, **kargs) +
-                specify(names="ZZ_2l2q", color=r.kRed, **kargs) +
-                specify(names="tt_bbll", color=28, **kargs) +
+        nFilesMax = 20
+        return (specify(names="H300_hh_bbtautau", color=r.kBlue, weights=w, nFilesMax=nFilesMax) +
+                specify(names="ZZ_2l2q", color=r.kRed, weights=w, nFilesMax=nFilesMax) +
+                specify(names="tt_bbll", color=28, weights=w, nFilesMax=nFilesMax) +
                 []
                 )
 
@@ -78,7 +108,7 @@ class example(supy.analysis):
                      rowColors=[r.kBlack, r.kViolet+4],
 
                      doLog=True,
-                     pegMinimum=0.3,
+                     pegMinimum=0.1,
                      showStatBox=False,
                      latexYieldTable=False,
                      ).plotAll()
