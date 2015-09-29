b89cba079d17d013ec8d46a91e63f77210af7871
Edward Laird <elaird@gmail.com>
2015-09-29 15:10:50 -0400

    make step specific to method; add spread profiles

--- steps.py
+++ steps.py
@@ -11,26 +11,29 @@ class visHistos(supy.analysisStep):
 
 
 class svHistos(supy.analysisStep):
-    def __init__(self, met="", svs=[]):
+    def __init__(self, met="", sv=""):
         self.met = met
-        self.svs = svs
+        self.sv = sv
 
     def uponAcceptance(self, s):
         mMin = 0.0
         mMax = 400.0
 
-        for sv in self.svs:
-            prefix = "%s_sv%s" % (self.met, sv)
-            best = s["%s_mass" % prefix]
-            uncSym = s["%s_massUncert" % prefix]
-            uncSymRel = uncSym / best
-            gen = s["gtMass"]
-            pull = (best - gen) / uncSym
+        prefix = "%s_sv%s" % (self.met, self.sv)
+        best = s["%s_mass" % prefix]
+        uncSym = s["%s_massUncert" % prefix]
+        uncSymRel = uncSym / best
+        gen = s["gtMass"]
+        pull = (best - gen) / uncSym
 
-            self.book.fill(best, "sv_best_%s" % sv, 100, mMin, mMax, title=";%s sv fit mass (GeV);Events / bin" % sv)
-            self.book.fill((gen, best), "sv_best_%s_vs_gen" % sv, (100, 100), (mMin, mMin), (mMax, mMax), title="gen mass (GeV);%s sv fit mass (GeV);Events / bin" % sv)
-            self.book.fill(uncSymRel, "sv_reluncsym_%s" % sv, 100, 0.0, 2.0, title=";%s sv rel. unc. sym.;Events / bin" % sv)
-            self.book.fill(pull, "sv_pull_%s" % sv, 40, -5.0, 5.0, title=";%s sv:  (m - gen) / uncSym;Events / bin" % sv)
+        self.book.fill(best, "%s_best" % prefix, 100, mMin, mMax, title=";%s fit mass (GeV);Events / bin" % prefix)
+        self.book.fill((gen, best), "%s_best_vs_gen" % prefix, (100, 100), (mMin, mMin), (mMax, mMax), title="gen mass (GeV);%s fit mass (GeV);Events / bin" % prefix)
+        self.book.fill(uncSymRel, "%s_reluncsym" % prefix, 100, 0.0, 2.0, title=";%s rel. unc. sym.;Events / bin" % prefix)
+        self.book.fill(pull, "%s_pull" % prefix, 40, -5.0, 5.0, title=";%s:  (m - gen) / uncSym;Events / bin" % prefix)
 
-            profBins = [0] + range(5, 125, 10) + range(125, 250, 25) + range(250, 450, 50)
-            self.book.fillVarBin((gen, best), "sv_best_%s_vs_gen_prof" % sv, profBins, title=";gen mass (GeV);%s sv fit mass (GeV)" % sv)
+        profBins = [0] + range(5, 125, 10) + range(125, 250, 25) + range(250, 450, 50)
+        self.book.fillVarBin((gen, best), "%s_best_vs_gen_prof" % prefix, profBins, title=";gen mass (GeV);%s fit mass (GeV)" % prefix)
+        self.book.fillVarBin((gen, best), "%s_best_vs_gen_profs" % prefix, profBins, title=";gen mass (GeV);%s fit mass (GeV)  (spread)" % prefix, options="s")
+
+        self.book.fillVarBin((gen, pull), "%s_pull_vs_gen_prof" % prefix, profBins, title=";gen mass (GeV);%s:  (m - gen) / uncSym" % prefix)
+        self.book.fillVarBin((gen, pull), "%s_pull_vs_gen_profs" % prefix, profBins, title=";gen mass (GeV);%s:  (m - gen) / uncSym  (spread)" % prefix, options="s")
